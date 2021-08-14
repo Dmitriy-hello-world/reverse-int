503610ca8e704c66123715893c30ecf26532346b
Dmitriy <easportstng@gmail.com>
2021-08-14 13:25:03 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    const arr = `${num}`.split('');
+    let result = '';
+    if(arr[0] == '-' || arr[0] == '0') {
+        arr.shift();
+    }
+    for(let i = arr.length - 1; i >= 0; i--) {
+        result += arr[i];
+    }
+    return +result;
 }
